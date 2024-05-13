591ba367f411fbd4701b8eaee37e41f721ce7848
Jlaborne <laborne.j@hotmail.fr>
2024-05-13 12:16:00 +0000

    task5

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    no_c_string = ""
+    for char in my_string:
+        if char != C or char != c:
+            result += char
+    return no_c_string
+    
