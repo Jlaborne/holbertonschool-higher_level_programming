5569b6b6588ae5c0cfeb466c9ea3d9c818ded8a8
Jlaborne <laborne.j@hotmail.fr>
2024-05-13 12:21:46 +0000

    task5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     no_c_string = ""
     for char in my_string:
-        if char != C or char != c:
-            result += char
+        if char != 'C' and char != 'c':
+            no_c_string += char
     return no_c_string
     
