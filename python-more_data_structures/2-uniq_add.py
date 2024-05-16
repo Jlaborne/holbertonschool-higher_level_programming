66576534c671157d12323b3ce54954d5e80dd04f
Jlaborne <laborne.j@hotmail.fr>
2024-05-16 14:41:39 +0200

    pycodestyle

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import functools
 
+
 def uniq_add(my_list=[]):
     return (functools.reduce(lambda a, b: a + b, set(my_list)))
--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def only_diff_elements(set_1, set_2):
     diff_elements = set()
     for elem in set_1:
