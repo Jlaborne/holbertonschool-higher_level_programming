fde713be1402cbb9b74f0ceceadd92667f61ae1c
Jlaborne <laborne.j@hotmail.fr>
2024-05-17 13:19:16 +0200

    3

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    printed_count = 0
+    try:
+        for item in my_list:
+            if printed_count < x:
+                try:
+                    print("{:d}".format(item), end=' ')
+                    printed_count += 1
+                except ValueError:
+                    pass
+        print()
+    except TypeError:
+        pass
+    return printed_count
