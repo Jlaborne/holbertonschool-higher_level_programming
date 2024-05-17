028fb228bbe1165421d7adf890052987ee207da1
Jlaborne <laborne.j@hotmail.fr>
2024-05-17 13:40:47 +0200

    1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,8 +3,7 @@
 
 def safe_print_integer(value):
     try:
-        int_value = int(value)
-        print("{:d}".format(int_value))
-        return True
-    except ValueError:
-        return False
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
