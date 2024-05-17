53d578cbf82f524f863c90c7b72406673a7e2277
Jlaborne <laborne.j@hotmail.fr>
2024-05-17 13:45:19 +0200

    2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    printed_count = 0
-    try:
-        for item in my_list:
-            if printed_count < x:
-                try:
-                    print("{:d}".format(item), end='')
-                    printed_count += 1
-                except ValueError:
-                    pass
-        print()
-        if x > len(my_list):
-            raise IndexError("x is greater than the length of my_list")
-    except TypeError:
-        pass
-    return printed_count
+    ret = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (ret)
