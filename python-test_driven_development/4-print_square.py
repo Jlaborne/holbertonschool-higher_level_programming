13ad9c49dd2ea67b755e5e0f1d4dc0f35c6bf385
Jlaborne <laborne.j@hotmail.fr>
2024-05-22 12:17:33 +0200

    4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -16,5 +16,5 @@ def print_square(size):
         raise ValueError("size must be >= 0")
 
     for i in range(size):
-        [print("#", end="") for j in range(size)]
+        [print("#", end="") for x in range(size)]
         print("")
