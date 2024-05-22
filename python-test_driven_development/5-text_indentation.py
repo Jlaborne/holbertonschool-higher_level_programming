12045c310556f61acd201b7b239776e1be252898
Jlaborne <laborne.j@hotmail.fr>
2024-05-22 11:33:24 +0200

    5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -20,9 +20,10 @@ def text_indentation(text):
     while c < len(text):
         print(text[c], end="")
         if text[c] == "\n" or text[c] in ".?:":
-            print("\n")
-        c += 1
-        while c < len(text) and text[c] == ' ':
+            if text[c] in ".?:":
+                print("\n")
             c += 1
-        continue
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
     c += 1
