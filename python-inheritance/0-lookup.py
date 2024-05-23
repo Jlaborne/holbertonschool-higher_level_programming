49a18f11480fc4090da76223c596cdb9bd828b96
Jlaborne <laborne.j@hotmail.fr>
2024-05-23 12:58:19 +0200

    task1

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Function that return a list of available attributes/method of an object"""
 
+
 def lookup(obj):
     """
     Args:
         obj: Object to return the list of attributes
-    Return: 
+    Return:
         li: the list that contain attributes/method
     """
     li = dir(obj)
