2d81fe1c5f686aa1cdbc11134bebd4aad93e2dd0
Jlaborne <laborne.j@hotmail.fr>
2024-05-23 13:44:42 +0200

    2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Check if object is an instance of a class"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Arg:
+        obj: an object
+        a_class: a class
+    Return:
+        True if obj is an instance of the class
+        False if not
+    """
+    if isinstance(obj, a_class):
+        return True
