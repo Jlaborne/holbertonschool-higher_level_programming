0a7808f2c4d1aa45ce29b7b4603719f838582b8a
Jlaborne <laborne.j@hotmail.fr>
2024-05-24 11:19:58 +0200

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define a class BaseGeometry"""
+
+
+class BaseGeometry():
+    """Represent BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
