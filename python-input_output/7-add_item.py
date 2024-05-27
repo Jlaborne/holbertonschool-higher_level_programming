bf306909da62cac9cef351d41ea996252013ef24
Jlaborne <laborne.j@hotmail.fr>
2024-05-27 14:40:30 +0200

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""""""
+"""Add arguments to a python list and save them to a file"""
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import sys
