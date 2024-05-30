b039c0390224286ff3fc9eb28808521dd34df0c4
Jlaborne <laborne.j@hotmail.fr>
2024-05-30 16:55:32 +0200

    3

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -19,7 +19,7 @@ def serialize_to_xml(dictionary, filename):
 
     tree = ET.ElementTree(root)
 
-    tree.write(filename, encoding="utf-8")
+    tree.write(filename, encoding="utf-8", xml_declaration=True)
 
 def deserialize_from_xml(filename):
     """
